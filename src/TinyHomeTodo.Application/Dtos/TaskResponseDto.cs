e143bb2fbcc5582b45fe9d03605995ecef4d1a43
Nikolay Dolgopolov <dolgopolov.n.v@gmail.com>
2026-08-31 21:02:16 -0400

    chore: match TaskResponseDto field order to the spec

--- src/TinyHomeTodo.Application/Dtos/TaskResponseDto.cs
+++ src/TinyHomeTodo.Application/Dtos/TaskResponseDto.cs
@@ -4,7 +4,7 @@ public class TaskResponseDto
 {
     public Guid Id { get; set; }
     public string TaskDescription { get; set; } = string.Empty;
-    public bool Completed { get; set; }
-    public DateTime? DueDate { get; set; }
     public DateTime CreatedDate { get; set; }
+    public DateTime? DueDate { get; set; }
+    public bool Completed { get; set; }
 }
